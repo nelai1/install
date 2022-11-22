cf386aba6ef207dba8410d7658aa7831ef9becfe
oleg <olselig@gmail.com>
2022-11-22 23:48:07 +0100

    update

--- scripts/nvim.py
+++ scripts/nvim.py
@@ -1,51 +1,50 @@
+import shutil
 import subprocess
 from pathlib import Path
 
-def ensure_bin():
-    bin_path = Path.home().resolve() / 'bin'
-    bin_path.mkdir(exist_ok=True)
-    return bin_path
-
-def ensure_config():
-    config = Path.home().resolve() / '.config'/ 'nvim'
-    config.mkdir(exist_ok=True)
-    return config / 'init.lua'
-
-class NVIM:
-    def __init__(self):
-        bin_path = ensure_bin()
-        self.path_out = bin_path / 'nvim'
-        self.config_path = ensure_config()
+URL_BIN = "https://github.com/neovim/neovim/releases/download/stable/nvim.appimage"
 
 
-    def download(self):
-        url = ('https://github.com/neovim/neovim/releases/'
-        'download/stable/nvim.appimage')
-        print('downloading latest appimage of nvim from master')
-        print(url)
-        cmd = ['wget', str(url), '-O', str(self.path_out), '-q']
-        _ = subprocess.call(cmd)
-        cmd = ['chmod', '+x', str(self.path_out)]
-        _ = subprocess.call(cmd)
-    
-    def setup_config(self):
-        url = ('https://raw.githubusercontent.com/nelai1/'
-                'install/main/nvim/.config/nvim/init.lua')
-        print('downloading latest config from master')
-        print(url)
-        cmd = ['wget', str(url), '-O', str(self.config_path), '-q']
-        _ = subprocess.call(cmd)
+def config_path() -> Path:
+    return Path.home().resolve() / ".config" / "nvim" / "init.lua"
 
-            
 
-    def install(self):
-        self.download()
-        if self.config_path.exists():
-            print(f'\nConfig {self.config_path} already exist. '
-                      '\nRemove to get a fresh version\n')
-        else:
-            self.setup_config()
+def bin_path() -> Path:
+    return Path.home().resolve() / "bin" / "nvim"
 
-if __name__ == '__main__':
-    nvim = NVIM()
-    nvim.install()
+
+def local_share() -> Path:
+    return Path.home().resolve() / ".local" / "share" / "nvim"
+
+
+def ensure_clean_base_dirs():
+    for p in [config_path(), bin_path()]:
+        p.parent.mkdir(exist_ok=True)
+        p.unlink()
+    shutil.rmtree(local_share())
+
+
+def download() -> None:
+    print("downloading latest appimage of nvim from master")
+    print(URL_BIN)
+    cmd = ["wget", str(URL_BIN), "-O", str(bin_path()), "-q"]
+    _ = subprocess.call(cmd)
+    cmd = ["chmod", "+x", str(bin_path())]
+    _ = subprocess.call(cmd)
+
+
+def setup_config():
+    URL_CONFIG = (
+        "https://raw.githubusercontent.com/nelai1/"
+        "install/main/nvim/.config/nvim/init.lua"
+    )
+    print("downloading latest config from master")
+    print(URL_CONFIG)
+    cmd = ["wget", str(URL_CONFIG), "-O", str(config_path()), "-q"]
+    _ = subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    ensure_clean_base_dirs()
+    download()
+    setup_config()
