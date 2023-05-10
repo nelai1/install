c160aaed7ebf66411e0dba59ecffd22e111e1b96
oleg <olselig@gmail.com>
2023-05-10 09:20:19 +0200

    install latest

--- installs/nerdfont.sh
+++ installs/nerdfont.sh
@@ -8,6 +8,7 @@
 FONT='Hack'
 TMP_ZIP_PATH="/tmp/${FONT}.zip"
 mkdir -p ~/.local/share/fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/"$FONT".zip -O "$TMP_ZIP_PATH"
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"$FONT".zip -O "$TMP_ZIP_PATH"
 unzip  "$TMP_ZIP_PATH" -d ~/.local/share/fonts/hack
 rm "$TMP_ZIP_PATH"
+fc-cache -fv
